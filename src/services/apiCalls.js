f87868b0594948a4b0b910a8d8753609a069e5fd
jordijerico <jordijerico@gmail.com>
2023-05-07 21:57:17 +0200

    Test to deploy. Connecting to back

--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const root = "https://backendproyectofinal-production.up.railway.app"
+const root = "http://localhost:4000"
 
 export const loginMe = async (body) => {
     return await axios.post(`${root}/auth/login`, body)
